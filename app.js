ebd779a5968a8a11feba5c46aac2961034c9c741
Heruixiao <fxzhrx@163.com>
2018-12-06 20:34:52 +0800

    练习

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //引入模块
 const express = require ("express"); //express
-const bodyParser = require ("body-parser");  //body-parser
+const bodyParser = require('body-parser');  //body-parser
 const mypro = require ("./routes/mypro.js");  //引入mypor的路由
 /*-----------------------------------------*/
 //创建web服务器
@@ -8,14 +8,14 @@ var server = express();
 //设置并监听服务器端口（3000）
 server.listen (3000);
 /*-----------------------------------------*/
-//托管静态资源
-server.use (express.static("mypro")); //mypro目录下
-/*-----------------------------------------*/
 //使用body-parser中间件将post请求数据解析为对象
 //注意：一定要写在路由的前边
 server.use (bodyParser.urlencoded({
   extended:false
 }));
+/*-----------------------------------------*/
+//托管静态资源
+server.use (express.static("mypro")); //mypro目录下
 /*-----------------------------------------*/
 //把ajax路由挂载
 server.use ("/mypro",mypro)
--- routes/mypro.js
+++ routes/mypro.js
@@ -1,2 +1,41 @@
 //引入上一级目录下的mysql模块
-const pool = require ("../pool.js")
+const pool = require('../pool.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+//创建空路由器
+var router = express.Router();
+
+//添加路由器
+router.post ("/login",(req,res)=>{
+	//获取用户名密码
+	var $uname = req.body.uname;
+	var $upwd = req.body.upwd;
+	console.log($uname,$upwd)
+	if (!$uname) {
+		res,send("请输入用户名");
+		return;
+	}
+	if (!$upwd) {
+		res.send("请输入输入密码");
+		return;
+	}
+	var sql = "select * from xz_user where uname = ? and upwd=?";
+	pool.query(sql,[$uname,$upwd],(err,result)=>{
+		if (err) throw err;
+		console.log(result)//检查masql值是否正常
+		//如果result.length大于0遍
+		if (result.length>0) {
+			res.send(result)
+		}else{
+			res.send("对不起！登录失败！！");
+		}
+	})
+})
+
+
+
+
+
+
+//导出路由器
+module.exports=router;
